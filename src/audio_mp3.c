483c603dfe1a911252b7e2bb5b4df678e983bb38
Guillaume Lecerf <foxcore@gmail.com>
2007-08-19 01:47:36 +0200

    remove references to libplayer

--- src/audio_mp3.c
+++ src/audio_mp3.c
@@ -4,18 +4,18 @@
  *
  * This file is part of libdlna.
  *
- * libplayer is free software; you can redistribute it and/or
+ * libdlna is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
  *
- * libplayer is distributed in the hope that it will be useful,
+ * libdlna is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public
- * License along with libplayer; if not, write to the Free Software
+ * License along with libdlna; if not, write to the Free Software
  * Foundation, Inc, 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
