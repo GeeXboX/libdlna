111f94e5ba35a9c057c825517ffd4660757e8de5
Benjamin Zores <ben@geexbox.org>
2007-11-01 12:55:31 +0100

    asf is not a supported avc file extension

--- src/av_mpeg4_part10.c
+++ src/av_mpeg4_part10.c
@@ -95,7 +95,7 @@ probe_avc (AVFormatContext *ctx)
 
 dlna_registered_profile_t dlna_profile_av_mpeg4_part10 = {
   .id = DLNA_PROFILE_AV_MPEG4_PART10,
-  .extensions = "mp4,3gp,3gpp,asf,mpg,mpeg,mpe,mp2t,ts",
+  .extensions = "mp4,3gp,3gpp,mpg,mpeg,mpe,mp2t,ts",
   .probe = probe_avc,
   .next = NULL
 };
