e35a5651098c3e9f3fd6719a328275372433448d
Benjamin Zores <ben@geexbox.org>
2007-11-01 14:30:43 +0100

    declare AVC profiles with 3GPP container

--- src/av_mpeg4_part10.c
+++ src/av_mpeg4_part10.c
@@ -553,6 +553,52 @@ static dlna_profile_t avc_ts_bl_cif15_bsac_iso = {
   .label = LABEL_VIDEO_CIF15
 };
 
+/******************/
+/* 3GPP Container */
+/******************/
+
+static dlna_profile_t avc_3gpp_bl_cif30_amr_wbplus = {
+  .id = "AVC_3GPP_BL_CIF30_AMR_WBplus",
+  .mime = MIME_VIDEO_3GP,
+  .label = LABEL_VIDEO_CIF30
+};
+
+static dlna_profile_t avc_3gpp_bl_cif15_amr_wbplus = {
+  .id = "AVC_3GPP_BL_CIF15_AMR_WBplus",
+  .mime = MIME_VIDEO_3GP,
+  .label = LABEL_VIDEO_CIF15
+};
+
+static dlna_profile_t avc_3gpp_bl_qcif15_aac = {
+  .id = "AVC_3GPP_BL_QCIF15_AAC",
+  .mime = MIME_VIDEO_3GP,
+  .label = LABEL_VIDEO_QCIF15
+};
+
+static dlna_profile_t avc_3gpp_bl_qcif15_aac_ltp = {
+  .id = "AVC_3GPP_BL_QCIF15_AAC_LTP",
+  .mime = MIME_VIDEO_3GP,
+  .label = LABEL_VIDEO_QCIF15
+};
+
+static dlna_profile_t avc_3gpp_bl_qcif15_heaac = {
+  .id = "AVC_3GPP_BL_QCIF15_HEAAC",
+  .mime = MIME_VIDEO_3GP,
+  .label = LABEL_VIDEO_QCIF15
+};
+
+static dlna_profile_t avc_3gpp_bl_qcif15_amr_wbplus = {
+  .id = "AVC_3GPP_BL_QCIF15_AMR_WBplus",
+  .mime = MIME_VIDEO_3GP,
+  .label = LABEL_VIDEO_QCIF15
+};
+
+static dlna_profile_t avc_3gpp_bl_qcif15_amr = {
+  .id = "AVC_3GPP_BL_QCIF15_AMR",
+  .mime = MIME_VIDEO_3GP,
+  .label = LABEL_VIDEO_QCIF15
+};
+
 static dlna_profile_t *
 probe_avc (AVFormatContext *ctx)
 {
