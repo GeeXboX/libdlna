a9faf954679626329c960c4ace3119bc3b6b5138
Benjamin Zores <ben@geexbox.org>
2007-11-10 17:11:17 +0100

    public api has changed: bump library's minor's number

--- src/dlna.h
+++ src/dlna.h
@@ -37,8 +37,8 @@ extern "C" {
 #define DLNA_STRINGIFY(s)         DLNA_TOSTRING(s)
 #define DLNA_TOSTRING(s) #s
 
-#define LIBDLNA_VERSION_INT  ((0<<16)+(1<<8)+4)
-#define LIBDLNA_VERSION      0.1.4
+#define LIBDLNA_VERSION_INT  ((0<<16)+(2<<8)+0)
+#define LIBDLNA_VERSION      0.2.0
 #define LIBDLNA_BUILD        LIBDLNA_VERSION_INT
 
 #define LIBDLNA_IDENT        "DLNA " DLNA_STRINGIFY(LIBDLNA_VERSION)
