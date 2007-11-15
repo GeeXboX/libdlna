c193347030b5b38e95bd34fc87724a2e977d8efe
Benjamin Zores <ben@geexbox.org>
2007-11-15 21:03:43 +0100

    add function to generate valid DMS UPnP device description string

--- src/dlna.h
+++ src/dlna.h
@@ -246,6 +246,46 @@ char * dlna_write_protocol_info (dlna_protocol_info_type_t type,
                                  dlna_org_flags_t flags,
                                  dlna_profile_t *p);
 
+/**
+ * Create a valid UPnP device description for Digital Media Server (DMS).
+ *
+ * @param[in] friendly_name      UPnP device friendly name.
+ * @param[in] manufacturer       UPnP device manufacturer.
+ * @param[in] manufacturer_url   UPnP device manufacturer URL.
+ * @param[in] model_description  UPnP device model description.
+ * @param[in] model_name         UPnP device model name.
+ * @param[in] model_number       UPnP device model number.
+ * @param[in] model_url          UPnP device model URL.
+ * @param[in] serial_number      UPnP device serial number.
+ * @param[in] uuid               UPnP device unique identifier.
+ * @param[in] presentation_url   UPnP device web presentation page URL.
+ * @param[in] cms_scpd_url       UPnP ConnectionManager service SCPD URL.
+ * @param[in] cms_control_url    UPnP ConnectionManager service control URL.
+ * @param[in] cms_event_url      UPnP ConnectionManager service event URL.
+ * @param[in] cds_scpd_url       UPnP ContentDirectory service SCPD URL.
+ * @param[in] cds_control_url    UPnP ContentDirectory service control URL.
+ * @param[in] cds_event_url      UPnP ContentDirectory service event URL.
+ * 
+ * @return                       The DMS device description string.
+ */
+char *
+dlna_dms_description_get (const char *friendly_name,
+                          const char *manufacturer,
+                          const char *manufacturer_url,
+                          const char *model_description,
+                          const char *model_name,
+                          const char *model_number,
+                          const char *model_url,
+                          const char *serial_number,
+                          const char *uuid,
+                          const char *presentation_url,
+                          const char *cms_scpd_url,
+                          const char *cms_control_url,
+                          const char *cms_event_url,
+                          const char *cds_scpd_url,
+                          const char *cds_control_url,
+                          const char *cds_event_url);
+
 #ifdef __cplusplus
 #if 0 /* avoid EMACS indent */
 {
