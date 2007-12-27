3b5725900a07fc61ea78b60d7117a94c1b693303
Benjamin Zores <ben@geexbox.org>
2007-12-27 23:54:33 +0100

    add upnp action request handler

--- src/upnp.c
+++ src/upnp.c
@@ -46,6 +46,83 @@
 #define UPNP_MAX_CONTENT_LENGTH 4096
 #define VIRTUAL_DIR "/web"
 
+static void
+upnp_action_request_handler (dlna_t *dlna, struct Upnp_Action_Request *ar)
+{
+#if 0
+  struct service_t *service;
+  struct service_action_t *action;
+#endif
+  char val[256];
+  uint32_t ip;
+
+  if (!dlna || !ar)
+    return;
+
+  if (ar->ErrCode != UPNP_E_SUCCESS)
+    return;
+
+  /* ensure that message target is the specified device */
+  if (strcmp (ar->DevUDN + 5, dlna->uuid))
+    return;
+  
+  ip = ar->CtrlPtIPAddr.s_addr;
+  ip = ntohl (ip);
+  sprintf (val, "%d.%d.%d.%d",
+           (ip >> 24) & 0xFF, (ip >> 16) & 0xFF, (ip >> 8) & 0xFF, ip & 0xFF);
+
+  if (dlna->verbosity == DLNA_MSG_INFO)
+  {
+    DOMString str = ixmlPrintDocument (ar->ActionRequest);
+    dlna_log (dlna, DLNA_MSG_INFO,
+              "***************************************************\n");
+    dlna_log (dlna, DLNA_MSG_INFO,
+              "**             New Action Request                **\n");
+    dlna_log (dlna, DLNA_MSG_INFO,
+              "***************************************************\n");
+    dlna_log (dlna, DLNA_MSG_INFO, "ServiceID: %s\n", ar->ServiceID);
+    dlna_log (dlna, DLNA_MSG_INFO, "ActionName: %s\n", ar->ActionName);
+    dlna_log (dlna, DLNA_MSG_INFO, "CtrlPtIP: %s\n", val);
+    dlna_log (dlna, DLNA_MSG_INFO, "Action Request:\n%s\n", str);
+    ixmlFreeDOMString (str);
+  }
+
+#if 0
+  /* not yet implemented */
+  if (find_service_action (ar, &service, &action))
+  {
+    struct action_event_t event;
+
+    event.request = ar;
+    event.status  = true;
+    event.service = service;
+
+    if (action->function (&event) && event.status)
+      ar->ErrCode = UPNP_E_SUCCESS;
+
+    if (dlna->verbosity == DLNA_MSG_INFO)
+    {
+      DOMString str = ixmlPrintDocument (ar->ActionResult);
+      dlna_log (dlna, DLNA_MSG_INFO, "Action Result:\n%s", str);
+      dlna_log (dlna, DLNA_MSG_INFO,
+                "***************************************************\n");
+      dlna_log (dlna, DLNA_MSG_INFO, "\n");
+      ixmlFreeDOMString (str);
+    }
+      
+    return;
+  }
+
+  if (service) /* Invalid Action name */
+    strcpy (ar->ErrStr, "Unknown Service Action");
+  else /* Invalid Service name */
+    strcpy (ar->ErrStr, "Unknown Service ID");
+#endif
+  
+  ar->ActionResult = NULL;
+  ar->ErrCode = UPNP_SOAP_E_INVALID_ACTION;
+}
+
 static int
 device_callback_event_handler (Upnp_EventType type,
                                void *event dlna_unused,
